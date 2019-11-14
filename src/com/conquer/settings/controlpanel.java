f836ae5126bf2c2f1baaf949809c9e2d384514ec
Herobuxx <herobuxx@gmail.com>
2019-11-14 20:09:59 +0700

    Init ConquerSettings
    Signed-off-by: Herobuxx <herobuxx@gmail.com>

--- src/com/conquer/settings/controlpanel.java
+++ src/com/conquer/settings/controlpanel.java
@@ -29,7 +29,7 @@ import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class IonIonizer extends SettingsPreferenceFragment {
+public class ControlPanel extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle icicle) {
