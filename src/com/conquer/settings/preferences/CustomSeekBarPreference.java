874b60f66625cfc9f2b02769abdbdb533f323271
Pranav Vashi <neobuddy89@gmail.com>
2020-01-08 21:11:15 +0700

    ControlPanel: Extend custom seekbar preferences
    Signed-off-by: Herobuxx <herobuxx@gmail.com>

--- src/com/conquer/settings/preferences/CustomSeekBarPreference.java
+++ src/com/conquer/settings/preferences/CustomSeekBarPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Dirty Unicorns Project
+ * Copyright (C) 2020 The ConquerOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
